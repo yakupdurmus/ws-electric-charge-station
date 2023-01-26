9bd0aa71491e96dcc2b3617c1764d41f906cbbaf
hebertcisco <hebertcisco@outlook.com>
2023-01-26 00:36:33 -0300

    refactor: :card_file_box: fix mongoDB config

--- src/infra/database/mongo.ts
+++ src/infra/database/mongo.ts
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 import {
-  DEFAULT_MONGO_URL,
   MONGODB_DATABASE,
   MONGODB_HOST,
   MONGODB_PASS,
@@ -9,12 +8,11 @@ import {
 
 const connectionString = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@${MONGODB_HOST}/${MONGODB_DATABASE}`;
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: 10, // use a connection pool with a size of 10 connections
-  keepAlive: true, // use a connection keep-alive
+const options: mongoose.ConnectOptions = {
+  keepAlive: true,
 };
+
+mongoose.set('strictQuery', true);
 
 export const database = () =>
   mongoose
